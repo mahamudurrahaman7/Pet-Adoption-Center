c0dbf857b8d7abb1f19a1bce77bee7ca652d4dea
mahamudurrahaman7 <mahamudurrahaman295@gmail.com>
2026-08-14 21:45:09 +0600

    first commit

---
+++ src/main/java/com/pet_adoption_center/PetAdoptionCenterApplication.java
@@ -0,0 +1,13 @@
+package com.pet_adoption_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetAdoptionCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetAdoptionCenterApplication.class, args);
+    }
+
+}
